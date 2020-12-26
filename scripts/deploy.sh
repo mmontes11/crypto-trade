4fcd207dce8098f1c667e9bafabd19114bad2133
Martín Montes <martin11lrx@gmail.com>
2020-12-26 20:21:40 +0000

    Added manifests for microservices deployment

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,10 +5,19 @@ set -e
 kubectl apply -f manifests/namespace.yml
 
 # Nats
+echo "🚀    Deploying Nats..."
 helm repo add nats https://nats-io.github.io/k8s/helm/charts/
 helm upgrade nats nats/nats -f manifests/nats.values.yml -n crypto-trade --install
 
 # ClickHouse
+echo "🚀    Deploying Clickhouse..."
 helm repo add liwenhe https://liwenhe1993.github.io/charts/
 helm upgrade clickhouse liwenhe/clickhouse -f manifests/clickhouse.values.yml -n crypto-trade --install
 
+# Microservices
+for ms in $(ls -d cmd/*); do
+    name=$(basename "$ms")
+    manifests="$ms/manifests"
+    echo "🚀    Deploying $name..."
+    kubectl apply -f "$manifests"
+done
