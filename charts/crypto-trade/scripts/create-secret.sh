89f578d7e239b0fc431d5c38020589d977778554
Martín Montes <martin11lrx@gmail.com>
2021-03-21 13:29:01 +0100

    fix: minor improvements

--- charts/crypto-trade/scripts/create-secret.sh
+++ charts/crypto-trade/scripts/create-secret.sh
@@ -2,11 +2,11 @@
 
 set -euo pipefail
 
-# export CLICKHOUSE_URL='tcp://crypto-trade-clickhouse:9000?username=default&password=default&database=default'
-# export NATS_URL='nats://crypto-trade-nats:422'
+# export CLICKHOUSE_URL='tcp://crypto-trade-clickhouse:9000'
+# export NATS_URL='nats://crypto-trade-nats:4222'
 # export PERSONAL_ACCESS_TOKEN='PERSONAL_ACCESS_TOKEN'
 
 kubectl create secret generic crypto-trade \
-  --from-literal=CLICKHOUSE_URL='$CLICKHOUSE_URL' \
-  --from-literal=NATS_URL='$NATS_URL' \
-  --from-literal=MIGRATIONS_URL='github://mmontes11:$PERSONAL_ACCESS_TOKEN@mmontes11/crypto-trade/cmd/subscriber/model/migrations'
+  --from-literal=CLICKHOUSE_URL=$CLICKHOUSE_URL \
+  --from-literal=NATS_URL=$NATS_URL \
+  --from-literal=MIGRATIONS_URL=github://mmontes11:$PERSONAL_ACCESS_TOKEN@mmontes11/crypto-trade/cmd/subscriber/model/migrations
