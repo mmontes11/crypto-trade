3a03a8a6081eb2b2cc4d141d8c06cac3aee5d409
Martín Montes <martin11lrx@gmail.com>
2021-03-21 22:41:22 +0100

    feat: improvements in templates

--- cmd/api/config/config.go
+++ cmd/api/config/config.go
@@ -4,7 +4,7 @@ import "github.com/mmontes11/crypto-trade/internal/config"
 
 var (
 	// Port indicates the API service port. It uses default K8s service port env variable
-	Port = config.GetEnv("API_SERVICE_PORT", "10000")
+	Port = config.GetEnv("CRYPTO_TRADE_API_SERVICE_PORT", "10000")
 	// Env indicates the environment name
 	Env = config.GetEnv("ENV", "development")
 	// ClickHouseURL it the URL of the ClickHouse instance
