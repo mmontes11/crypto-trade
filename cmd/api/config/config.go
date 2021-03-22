44cee5fd81463d88ac530a2230292fa0f62610e6
Martín Montes <martin11lrx@gmail.com>
2021-03-22 21:43:29 +0100

    feat: changed api port variable

--- cmd/api/config/config.go
+++ cmd/api/config/config.go
@@ -3,10 +3,10 @@ package config
 import "github.com/mmontes11/crypto-trade/internal/config"
 
 var (
-	// Port indicates the API service port. It uses default K8s service port env variable
-	Port = config.GetEnv("CRYPTO_TRADE_API_SERVICE_PORT", "10000")
+	// Port indicates the API service port
+	Port = config.GetEnv("PORT", "10000")
 	// Env indicates the environment name
 	Env = config.GetEnv("ENV", "development")
 	// ClickHouseURL it the URL of the ClickHouse instance
-	ClickHouseURL = config.GetEnv("CLICKHOUSE_URL", "tcp://127.0.0.1:9000?username=crypto&password=crypto&database=crypto")
+	ClickHouseURL = config.GetEnv("CLICKHOUSE_URL", "tcp://127.0.0.1:9000")
 )
