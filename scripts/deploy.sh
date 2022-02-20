31a22a156bc322cca213972f599e1c15dad7f274
Martín Montes <martin11lrx@gmail.com>
2022-02-20 21:22:31 +0100

    Updated deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,9 +2,10 @@
 
 RELEASE="crypto-trade"
 REPO="mmontes"
+NAMESPACE="crypto-trade"
 
 helm repo add "$REPO" https://charts.mmontes-dev.duckdns.org
 helm repo update
 
 echo "🚀 Deploying '${RELEASE}'..."
-helm upgrade --install "$RELEASE" "$REPO/$RELEASE"
+helm upgrade --install "$RELEASE" "$REPO/$RELEASE" --namespace "$NAMESPACE"
